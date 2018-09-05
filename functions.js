4eb2b70adbf9856b56581ce48111a146eb5dfda9
rohegde7 <hegde.rohit7@gmail.com>
2018-09-05 18:17:45 +0530

    all the JS functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+function fun_innerHtml(){
+    document.getElementById('text1').innerHTML="Function call Success!"
+}
+ 
+function fun_documentWrite(){
+    document.write("Habibi clear all")
+}
+
+function fun_windowAlert(){
+    window.alert("You just created an alert!")
+}
+
+function fun_consoleLog(){
+    var e = document.getElementById('text1').textContent;
+    console.log(e)
+}
+
